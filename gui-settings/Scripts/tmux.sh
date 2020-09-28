cb192e25c0914d0e85e58ec4371782db20064f27
wtchrs <wtchr_@hotmail.com>
2020-09-28 22:10:57 +0900

    2020-09-28 Some settings for Tmux

---
+++ gui-settings/Scripts/tmux.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+WHOAMI=$(whoami)
+SESSION_NAME=${WHOAMI}
+
+if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+	tmux -2 attach-session -t $SESSION_NAME
+else
+	tmux -2 new-session -s $SESSION_NAME
+fi
+
