7f1c293fa5be946700daca81ef21488d83ab7cae
wtchrs <wtchr_@hotmail.com>
2020-12-29 13:35:17 +0000

    2020-12-29-3

--- settings/config/polybar/launch.sh
+++ settings/config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,8 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar white &
-#polybar bar2 &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-i3 &
+done
 
 echo "Bars launched..."
