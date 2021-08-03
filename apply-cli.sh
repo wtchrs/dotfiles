94b1fd974b0fa6a1a5c1176ca648e3e3ca5ce82f
wtchrs <wtchr_@hotmail.com>
2021-08-03 20:24:56 +0900

    Add fzf plugin for zsh autocomplete

--- apply-cli.sh
+++ apply-cli.sh
@@ -2,7 +2,6 @@
 
 cp ./settings/vimrc ~/.vimrc
 cp ./settings/tmux.conf ~/.tmux.conf
-cp ./settings/status-line.tmux ~/.status-line.tmux
 cp ./settings/zshrc ~/.zshrc
 
 cp -r ./settings/config/fish ~/.config
