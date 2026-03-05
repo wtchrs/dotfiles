0a106b19d86b046ada2b755c166e17609e617055
wtchrs <wtchr_@hotmail.com>
2026-03-06 01:22:35 +0900

    chore(stow): remove unused packages

--- hyprland/.scripts/reload-quickshell.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-killall -9 quickshell
-sleep 1
-quickshell &
--- waybar/.scripts/waybar-calendar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-RULE="[float; size 250 250; center]"
-COMMAND="alacritty -e ~/.config/waybar/scripts/calendar-internal.sh"
-
-hyprctl dispatch exec "${RULE} ${COMMAND}"
